a61bc44905af05196a1b0906ae66c3bd88da0cf6
CedricTheveneau <theveneaucedric@gmail.com>
2025-02-07 16:32:11 +0100

    DeepScan issue fix
     - [X] Fixed the following issue : https://deepscan.io/dashboard/#view=project&pid=28807&iid=210505084&tid=23515&subview=pull-request&prid=2074094

--- app/emailConfirmation/[token]/page.js
+++ app/emailConfirmation/[token]/page.js
@@ -1,14 +1,13 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useGlobalContext } from "../../layout/GlobalContext";
 
 export default function Login({ params }) {
-  const { apiGateway } =
-  useGlobalContext();
+  const { apiGateway } = useGlobalContext();
   const { token } = params;
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -23,19 +22,18 @@ export default function Login({ params }) {
             },
           }
         );
-  
+
         if (!confirmationResponse.ok) {
           const response = await confirmationResponse.json();
-        setData(response);
+          setData(response);
           throw new Error(
             "Une erreur est survenue lors de la vérification de votre adresse email"
           );
         }
-  
+
         const response = await confirmationResponse.json();
         console.log(response);
-        
-  
+
         setData(response);
       } catch (error) {
         console.error(
@@ -43,33 +41,67 @@ export default function Login({ params }) {
           error
         );
       }
-    }
-    confirmEmail()
+    };
+    confirmEmail();
 
     setTimeout(() => {
-      router.push('/login')
-    }, 15000)
-  }, [token])
+      router.push("/login");
+    }, 15000);
+  }, [token]);
 
   return (
-
     <main className="home">
       <div className="heading">
-      {data !== null ? <><h1>{data.message.title && data.message.title}</h1>
-      <p>{data.message.content ? data.message.content : data.message}</p>
-      {data.message.content ? <>
-      <p>Vous allez être redirigé très prochainement vers la page de connexion.</p>
-      <p>Si après 15 secondes, vous n&apos;avez pas été redirigé vers la page de connexion, cliquez sur le lien ci-dessous.</p>
-      <Link className="link" href="/login">Je me connecte !</Link>
-      </>
-      : data.message !== null &&
-      <>
-      <p>Une erreur s&apos;est produite lors de la vérification de votre compte. Veuillez réessayer ultérieurement</p>
-      <p>Vous serez redirigé vers la page de connexion d&apos;ici 15 secondes. Passé ce délai, si vous n&apos;avez pas été redirigé, cliquez sur le lien ci-dessous pour retourner à l&apos;accueil.</p>
-      <Link className="link" href="/">Je retourne à l&apos;accueil !</Link>
-      </>
-      }
-      </> : <p>Confirmation de l&apos;email en cours...</p>}
+        {data !== null ? (
+          <>
+            {data.message ? (
+              <>
+                <h1>{data.message.title}</h1>
+                <p>
+                  {data.message.content ? data.message.content : data.message}
+                </p>
+                {data.message.content ? (
+                  <>
+                    <p>
+                      Vous allez être redirigé très prochainement vers la page
+                      de connexion.
+                    </p>
+                    <p>
+                      Si après 15 secondes, vous n&apos;avez pas été redirigé
+                      vers la page de connexion, cliquez sur le lien ci-dessous.
+                    </p>
+                    <Link className="link" href="/login">
+                      Je me connecte !
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <p>
+                      Une erreur s&apos;est produite lors de la vérification de
+                      votre compte. Veuillez réessayer ultérieurement
+                    </p>
+                    <p>
+                      Vous serez redirigé vers la page de connexion d&apos;ici
+                      15 secondes. Passé ce délai, si vous n&apos;avez pas été
+                      redirigé, cliquez sur le lien ci-dessous pour retourner à
+                      l&apos;accueil.
+                    </p>
+                    <Link className="link" href="/">
+                      Je retourne à l&apos;accueil !
+                    </Link>
+                  </>
+                )}
+              </>
+            ) : (
+              <p>
+                Erreur lors de la récupération des informations. Veuillez
+                réessayer.
+              </p>
+            )}
+          </>
+        ) : (
+          <p>Confirmation de l&apos;email en cours...</p>
+        )}
       </div>
     </main>
   );
